dcb74ed55f077e301be33f90e587fc1b2990bc95
Anand J <anandj@cs.toronto.edu>
2024-11-24 03:05:57 -0500

    Make LoopGen child of IRClone

--- include/reffine/pass/loopgen.h
+++ include/reffine/pass/loopgen.h
@@ -1,26 +1,16 @@
 #ifndef INCLUDE_REFFINE_PASS_LOOPGEN_H_
 #define INCLUDE_REFFINE_PASS_LOOPGEN_H_
 
-#include "reffine/pass/irgen.h"
+#include "reffine/pass/irclone.h"
 
 namespace reffine {
 
-class LoopGenCtx : public IRGenCtx<Sym, Expr> {
-public:
-    LoopGenCtx(shared_ptr<Func> op_func, shared_ptr<Func> loop_func) :
-        IRGenCtx(op_func->tbl, loop_func->tbl), loop_func(loop_func)
-    {}
+using LoopGenCtx = IRCloneCtx;
 
-private:
-    shared_ptr<Func> loop_func;
-
-    friend class LoopGen;
-};
-
-class LoopGen : public IRGen<Sym, Expr> {
+class LoopGen : public IRClone {
 public:
     explicit LoopGen(LoopGenCtx& ctx) :
-        IRGen(ctx), _ctx(ctx)
+        IRClone(ctx)
     {}
 
     static shared_ptr<Func> Build(shared_ptr<Func>);
@@ -37,19 +27,6 @@ private:
     Expr visit(Element&) final;
     Expr visit(Reduce&) final;
     void visit(Func&) final;
-
-    void visit(Stmts&) final { throw runtime_error("Operation not supported"); }
-    void visit(IfElse&) final { throw runtime_error("Operation not supported"); }
-    void visit(NoOp&) final { throw runtime_error("Operation not supported"); }
-    Expr visit(Alloc&) final { throw runtime_error("Operation not supported"); }
-    Expr visit(Load&) final { throw runtime_error("Operation not supported"); }
-    void visit(Store&) final { throw runtime_error("Operation not supported"); }
-    Expr visit(Loop&) final { throw runtime_error("Operation not supported"); }
-    Expr visit(IsValid&) final { throw runtime_error("Operation not supported"); }
-    Expr visit(SetValid&) final { throw runtime_error("Operation not supported"); }
-    Expr visit(FetchDataPtr&) final { throw runtime_error("Operation not supported"); }
-
-    LoopGenCtx& _ctx;
 };
 
 }  // namespace reffine
