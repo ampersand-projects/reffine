738eda51998fb3024c8a455f65abb5eb58cd8bf1
Anand J <anandj@cs.toronto.edu>
2024-12-09 19:31:03 -0500

    Reformat tests

--- test/src/test_base.cpp
+++ test/src/test_base.cpp
@@ -1,12 +1,14 @@
-#include "reffine/engine/engine.h"
 #include "test_base.h"
+
 #include <gtest/gtest.h>
+
+#include "reffine/engine/engine.h"
 
 using namespace reffine;
 
 void foo_test()
 {
-    auto out = 1+1;
+    auto out = 1 + 1;
     auto true_out = 2;
     ASSERT_EQ(out, true_out);
 }
