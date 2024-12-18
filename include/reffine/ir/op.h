3702f36d526c34a5e1fd79f6d3f9b44591d2122b
Anand J <anandj@cs.toronto.edu>
2024-12-17 20:27:07 -0500

    FIX: Element construction validation

--- include/reffine/ir/op.h
+++ include/reffine/ir/op.h
@@ -56,10 +56,10 @@ struct Element : public ExprNode {
         const auto& vtype = vec->type;
 
         // Indexing to a subspace in the vector is not supported yet.
-        ASSERT(vtype.dim == idxs.size());
+        ASSERT(vtype.dim == this->idxs.size());
 
-        for (size_t i = 0; i < idxs.size(); i++) {
-            ASSERT(vtype.dtypes[i] == idxs[i]->type);
+        for (size_t i = 0; i < this->idxs.size(); i++) {
+            ASSERT(vtype.dtypes[i] == this->idxs[i]->type);
         }
     }
 
