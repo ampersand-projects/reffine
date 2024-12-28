b463c8432fbbb402a18d1916187eda428e97447f
Anand J <anandj@cs.toronto.edu>
2024-12-27 23:58:31 -0500

    Hack

--- src/pass/reffinepass.cpp
+++ src/pass/reffinepass.cpp
@@ -44,8 +44,6 @@ IterSpace apply_bounds(IterSpace ispace, Expr pred)
 {
     ASSERT(ispace.space->type.is_val());
 
-    IterSpace ispace;
-
     if (auto lb = get_lower_bound(ispace.space, pred)) {
         ispace.lower_bound = make_shared<Max>(ispace.lower_bound, lb);
     } else if (auto ub = get_upper_bound(ispace.space, pred)) {
@@ -58,30 +56,30 @@ IterSpace apply_bounds(IterSpace ispace, Expr pred)
 }
 
 // Temporary implementation. Need to revisit
-IterSpace intersect(Sym iter, IterSpace a, IterSpace b)
+IterSpace intersect(IterSpace a, IterSpace b)
 {
     ASSERT(!(a.space->type.is_vector() && b.space->type.is_vector()));
 
-    IterSpace& left;
-    IterSpace& right;
+    IterSpace* left;
+    IterSpace* right;
 
     if (a.space->type.is_vector()) {
-        left = a;
-        right = b;
+        left = &a;
+        right = &b;
     } else {
-        left = b;
-        right = a;
+        left = &b;
+        right = &a;
     }
 
     IterSpace ispace;
 
-    ispace.space = left.space;
-    ispace.lower_bound = make_shared<Max>(left.lower_bound, right.lower_bound);
-    ispace.upper_bound = make_shared<Min>(left.upper_bound, right.upper_bound);
-    ispace.body_cond = make_shared<And>(left.body_cond, right.body_cond);
-    ispace.iter_to_idx = left.iter_to_idx;
-    ispace.idx_to_iter = left.idx_to_iter;
-    ispace.idx_incr = left.idx_incr;
+    ispace.space = left->space;
+    ispace.lower_bound = make_shared<Max>(left->lower_bound, right->lower_bound);
+    ispace.upper_bound = make_shared<Min>(left->upper_bound, right->upper_bound);
+    ispace.body_cond = make_shared<And>(left->body_cond, right->body_cond);
+    ispace.iter_to_idx = left->iter_to_idx;
+    ispace.idx_to_iter = left->idx_to_iter;
+    ispace.idx_incr = left->idx_incr;
 
     return ispace;
 }
@@ -95,8 +93,7 @@ IterSpace ReffinePass::visit(NaryExpr& e)
         case MathOp::LTE:
         case MathOp::GT:
         case MathOp::GTE:
-            auto ispace = eval(e.arg(0));
-            return apply_bounds(ispace, this->tmp_expr(e));
+            return apply_bounds(eval(e.arg(0)), this->tmp_expr(e));
         default:
             throw runtime_error("Operator not supported by ReffinePass");
     }
@@ -105,14 +102,14 @@ IterSpace ReffinePass::visit(NaryExpr& e)
 IterSpace ReffinePass::visit(Sym sym)
 {
     IterSpace ispace;
-    ispace.space = sym;
 
-    if (ispace.space == op().iters[0]) {
-        ispace.lower_bound = make_shared<Const>(iter->type.btype, -10000); // -Inf
-        ispace.upper_bound = make_shared<Const>(iter->type.btype, 10000); // +Inf
+    if (sym == op().iters[0]) {
+        ispace.space = sym;
+        ispace.lower_bound = make_shared<Const>(sym->type.btype, -10000); // -Inf
+        ispace.upper_bound = make_shared<Const>(sym->type.btype, 10000); // +Inf
         ispace.body_cond = make_shared<Const>(BaseType::BOOL, 1);
-        ispace.idx_to_iter = [iter](Expr idx) {
-            return make_shared<Cast>(iter->type, idx);
+        ispace.idx_to_iter = [sym](Expr idx) {
+            return make_shared<Cast>(sym->type, idx);
         };
         ispace.iter_to_idx = [](Expr iter) {
             return make_shared<Cast>(types::IDX, iter);
@@ -134,5 +131,10 @@ IterSpace ReffinePass::Build(Op& op)
 
     op.pred->Accept(rpass);
 
-    return rpass.val();
+    auto ispace = rpass.val();
+
+    // Removing body_cond for now
+    ispace.body_cond = nullptr;
+
+    return ispace;
 }
