e667417d2542e4f45795445e9268041e5514773a
Anand J <anandj@cs.toronto.edu>
2024-12-28 19:29:12 -0500

    Test case

--- test/src/test_reduce.cpp
+++ test/src/test_reduce.cpp
@@ -62,22 +62,27 @@ void aggregate_loop_test()
 shared_ptr<Func> vector_op()
 {
     auto t_sym = make_shared<SymNode>("t", types::INT64);
-    Op op(vector<Sym>{t_sym},
-          make_shared<And>(make_shared<GreaterThan>(
-                               t_sym, make_shared<Const>(BaseType::INT64, 0)),
-                           make_shared<LessThan>(
-                               t_sym, make_shared<Const>(BaseType::INT64, 10))),
-          vector<Expr>{t_sym});
+    auto vec_in_sym = make_shared<SymNode>("vec_in", types::VECTOR<1>(vector<DataType>{
+        types::INT64, types::INT64, types::INT64, types::INT64, types::INT64, types::INT8, types::INT64 }));
+    Op op(
+        vector<Sym>{t_sym},
+        make_shared<Element>(vec_in_sym, vector<Expr>{t_sym}),
+        vector<Expr>{
+            make_shared<Get>(make_shared<Element>(vec_in_sym, vector<Expr>{t_sym}), 1)
+        }
+    );
 
     auto sum = make_shared<Reduce>(
-        op, []() { return make_shared<Const>(BaseType::INT64, 0); },
-        [](Expr s, Expr v) {
+        op,
+        [] () { return make_shared<Const>(BaseType::INT64, 0); },
+        [] (Expr s, Expr v) {
             auto e = make_shared<Get>(v, 0);
             return make_shared<Add>(s, e);
-        });
+        }
+    );
     auto sum_sym = make_shared<SymNode>("sum", sum);
 
-    auto foo_fn = make_shared<Func>("foo", sum_sym, vector<Sym>{});
+    auto foo_fn = make_shared<Func>("foo", sum_sym, vector<Sym>{vec_in_sym});
     foo_fn->tbl[sum_sym] = sum;
 
     return foo_fn;
@@ -86,9 +91,10 @@ shared_ptr<Func> vector_op()
 void aggregate_op_test()
 {
     auto op = vector_op();
-    auto query_fn = compile_op<long (*)()>(op);
+    auto query_fn = compile_op<long (*)(void*)>(op);
 
-    auto res = query_fn();
+    auto tbl = get_input_vector();
+    auto res = query_fn(&tbl->array);
 
-    ASSERT_EQ(res, 45);
+    ASSERT_EQ(res, 131977);
 }
