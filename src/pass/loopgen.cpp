fcfa26dd94e952b0ad98b81cc46e30938fdaab0d
Anand J <anandj@cs.toronto.edu>
2024-12-29 22:13:09 -0500

    Rewrite

--- src/pass/loopgen.cpp
+++ src/pass/loopgen.cpp
@@ -83,6 +83,7 @@ Expr LoopGen::visit(Reduce& red)
         _store(state_addr, red.init()),
     });
     red_loop->body = _store(state_addr, red.acc(load_state_expr, red_loop->output));
+    red_loop->output = load_state_expr;
 
     return red_loop;
 }
