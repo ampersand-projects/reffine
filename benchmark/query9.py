7e9e822e32bbc4fd0bc105e944e284e517e6f482
Anand J <anandj@cs.toronto.edu>
2025-01-04 15:29:11 -0500

    Update query9.py with data types

--- benchmark/query9.py
+++ benchmark/query9.py
@@ -1,6 +1,7 @@
 import time
 import pyarrow as pa
 import pandas as pd
+import numpy as np
 
 def to_bucket(q: int):
     if 1 <= q <= 20:
@@ -36,8 +37,39 @@ def run_query9(df: pd.DataFrame) -> pd.DataFrame:
 
     return agg
 
+dtypes = {
+    "ss_item_sk": np.int32,
+    "ss_ticket_number": np.int64,
+    "ss_sold_date_sk": np.int32,
+    "ss_sold_time_sk": np.int32,
+    "ss_customer_sk": np.int32,
+    "ss_cdemo_sk": np.int32,
+    "ss_hdemo_sk": np.int32,
+    "ss_addr_sk": np.int32,
+    "ss_store_sk": np.int32,
+    "ss_promo_sk": np.int32,
+    "ss_quantity": np.int32,
+    "ss_wholesale_cost": np.float32,
+    "ss_list_price": np.float32,
+    "ss_sales_price": np.float32,
+    "ss_ext_discount_amt": np.float32,
+    "ss_ext_sales_price": np.float32,
+    "ss_ext_wholesale_cost": np.float32,
+    "ss_ext_list_price": np.float32,
+    "ss_ext_tax": np.float32,
+    "ss_coupon_amt": np.float32,
+    "ss_net_paid": np.float32,
+    "ss_net_paid_inc_tax": np.float32,
+    "ss_net_profit": np.float32,
+}
+
 # Table store_sales
-df = pd.read_csv("/home/anandj/data/code/reffine/benchmark/tpcds-data/store_sales_1_10.dat", delimiter="|", engine="pyarrow", names=["ss_item_sk","ss_ticket_number","ss_sold_date_sk","ss_sold_time_sk","ss_customer_sk","ss_cdemo_sk","ss_hdemo_sk","ss_addr_sk","ss_store_sk","ss_promo_sk","ss_quantity","ss_wholesale_cost","ss_list_price","ss_sales_price","ss_ext_discount_amt","ss_ext_sales_price","ss_ext_wholesale_cost","ss_ext_list_price","ss_ext_tax","ss_coupon_amt","ss_net_paid","ss_net_paid_inc_tax","ss_net_profit"]).fillna(value=0)
+df = pd.read_csv(
+    "/home/anandj/data/code/reffine/benchmark/tpcds-data/store_sales_1_10.dat",
+    delimiter="|",
+    engine="pyarrow",
+    names=list(dtypes.keys()),
+).fillna(value=0).astype(dtypes)
 #export_df("store_sales.arrow", df)
 
 start = time.time()
