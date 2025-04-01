9601562ced49faeac0b974be0bb2f8f88cf005c0
natalievolk <natalievolk@rogers.com>
2025-04-01 09:40:34 -0400

    Add more rapids benchmarks

--- benchmark/cuda-rapids/rapids.py
+++ benchmark/cuda-rapids/rapids.py
@@ -8,9 +8,7 @@ import time
 #   - https://github.com/natalievolk/reffine/tree/vectorize
 #   - https://developer.nvidia.com/rapids
 
-def benchmark_aggregate(n=1025, iter=1000):
-    
-    
+def benchmark_aggregate(n=1025, iter=10000):
     values = pd.Series(range(n))
     values_gpu = cudf.Series(values)
     df_gpu = cudf.DataFrame({"values": values_gpu})
@@ -19,12 +17,49 @@ def benchmark_aggregate(n=1025, iter=1000):
     for i in range(iter):
         start = time.time()
         
-        result_gpu = df_gpu.sum()
+        res_gpu = df_gpu.sum()
         
         total_time += time.time() - start
         
     av_time = total_time / iter
     print(f"Average RAPIDS cuDF aggregation time: {av_time:.6f} seconds over {iter} iterations")
-    print(result_gpu)
+    print(res_gpu)
+    
+    
+def benchmark_transform(n=1025, iter=10000):
+    values = pd.Series(range(n))
+    values_gpu = cudf.Series(values)
+    df_gpu = cudf.DataFrame({"values": values_gpu})
+
+    total_time = 0
+    for i in range(iter):
+        start = time.time()
+        
+        res_gpu = df_gpu + 1
+        
+        total_time += time.time() - start
+        
+    av_time = total_time / iter
+    print(f"Average RAPIDS cuDF transform time: {av_time:.6f} seconds over {iter} iterations")
+    print(res_gpu)
+    
+def benchmark_select(n=1025, iter=10000):
+    values = pd.Series(range(n))
+    values_gpu = cudf.Series(values)
+    df_gpu = cudf.DataFrame({"values": values_gpu})
+
+    total_time = 0
+    for i in range(iter):
+        start = time.time()
+        
+        res_gpu = df_gpu.map(lambda x: x if x % 2 == 0 else 0)
+        
+        total_time += time.time() - start
+        
+    av_time = total_time / iter
+    print(f"Average RAPIDS cuDF select time: {av_time:.6f} seconds over {iter} iterations")
+    print(res_gpu)
     
 benchmark_aggregate()
+benchmark_transform()
+benchmark_select()
