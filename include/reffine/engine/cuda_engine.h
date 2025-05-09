ad2717aa6fd9d6f948d493b541a2337c3deb8848
natalievolk <natalievolk@rogers.com>
2025-05-09 14:26:27 -0400

    Remove engine attribute

--- include/reffine/engine/cuda_engine.h
+++ include/reffine/engine/cuda_engine.h
@@ -24,8 +24,6 @@ class CudaEngine {
 public:
     CudaEngine() {}
 
-    static unique_ptr<CudaEngine> engine;
-
     static CudaEngine* Get();
     CUmodule Build(Module&);
     CUfunction Lookup(CUmodule, string);
