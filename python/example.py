8dbacdd0cd3847de3e6f72df763fdf9aaa710d68
natalievolk <natalievolk@rogers.com>
2025-06-20 15:16:18 -0400

    Make executing a query more generalizable

--- python/example.py
+++ python/example.py
@@ -4,6 +4,7 @@ import numpy as np
 
 def transform_fn(n):
     vec_in_sym = ir._sym("x", ir._i64_t.ptr())
+    vec_in_sym2 = ir._sym("z", ir._i64_t.ptr())
     vec_out_sym = ir._sym("y", ir._i64_t.ptr())
 
     length = ir._idx(n)
@@ -13,20 +14,23 @@ def transform_fn(n):
 
     val_ptr = ir._call("get_elem_ptr", ir._i64_t.ptr(), [vec_in_sym, idx])
     val = ir._load(val_ptr)
+    
+    val_ptr2 = ir._call("get_elem_ptr", ir._i64_t.ptr(), [vec_in_sym2, idx])
+    val2 = ir._load(val_ptr2)
 
     out_ptr = ir._call("get_elem_ptr", ir._i64_t.ptr(), [vec_out_sym, idx])
 
     loop = ir._loop(ir._load(vec_out_sym))
     loop.init = ir._stmts([idx_alloc, ir._store(idx_addr, ir._idx(0))])
     loop.body = ir._stmts([
-        ir._store(out_ptr, ir._add(ir._i64(1), val)),
+        ir._store(out_ptr, ir._add(ir._add(ir._i64(1), val), val2)),
         ir._store(idx_addr, ir._add(idx, ir._idx(1))),
     ])
     loop.exit_cond = ir._gte(idx, length)
 
     loop_sym = ir._sym("loop", loop)
 
-    fn = ir._func("foo", loop, [vec_out_sym, vec_in_sym], {}, True)
+    fn = ir._func("foo", loop, [vec_out_sym, vec_in_sym, vec_in_sym2], {}, False)
     fn.insert_sym(idx_addr, idx_alloc)
     
     return fn
@@ -38,4 +42,5 @@ print(f"Example LLVM IR:\n{exec.print_llvm(fn)}\n\n")
 
 out_arr = np.zeros(100, dtype=np.int64)
 in_arr = np.arange(100, dtype=np.int64)
-print(f"Loop execution:\n{exec.execute_loop(fn, out_arr, in_arr)}\n\n")
+in_arr2 = np.arange(100, dtype=np.int64)
+print(f"Loop execution:\n{exec.execute_loop(fn, out_arr, [in_arr, in_arr2])}\n\n")
