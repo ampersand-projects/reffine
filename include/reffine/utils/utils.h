d4bdbe845120097348eb5f0eb107a5c04ff68cdf
Anand J <anandj@cs.toronto.edu>
2025-07-30 21:11:59 -0400

    Removing some stuff for debugging

--- include/reffine/utils/utils.h
+++ include/reffine/utils/utils.h
@@ -18,23 +18,14 @@ using namespace reffine;
 template <typename T>
 T compile_loop(std::shared_ptr<Func> loop)
 {
-    LOG(INFO) << "Loop IR (raw):" << std::endl << loop->str() << std::endl;
     CanonPass::Build(loop);
-    LOG(INFO) << "Loop IR (canon):" << std::endl << loop->str() << std::endl;
     loop = LoadStoreExpand::Build(loop);
-    LOG(INFO) << "Loop IR (expand):" << std::endl << loop->str() << std::endl;
     loop = NewGetElimination::Build(loop);
-    LOG(INFO) << "Loop IR (eliminate):" << std::endl
-              << loop->str() << std::endl;
 
     auto jit = ExecEngine::Get();
     auto llmod = make_unique<llvm::Module>("test", jit->GetCtx());
     LLVMGen::Build(loop, *llmod);
-    LOG(INFO) << "LLVM IR (raw):" << std::endl
-              << IRPrinter::Build(*llmod) << std::endl;
     jit->Optimize(*llmod);
-    LOG(INFO) << "LLVM IR (optimized):" << std::endl
-              << IRPrinter::Build(*llmod) << std::endl;
     jit->AddModule(std::move(llmod));
     return jit->Lookup<T>(loop->name);
 }
@@ -42,7 +33,6 @@ T compile_loop(std::shared_ptr<Func> loop)
 template <typename T>
 T compile_op(std::shared_ptr<Func> op)
 {
-    LOG(INFO) << "Reffine IR:" << std::endl << op->str() << std::endl;
     auto loop = LoopGen::Build(op);
     return compile_loop<T>(loop);
 }
