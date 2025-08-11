40ecabcf58da4b938825e0b73ed63946e7cf2543
Anand J <anandj@cs.toronto.edu>
2025-08-11 12:53:04 -0400

    Add make_vector external vinstr

--- src/pass/vinstr/external.cpp
+++ src/pass/vinstr/external.cpp
@@ -1,6 +1,14 @@
 #include "reffine/pass/vinstr/vinstr.h"
+#include "reffine/arrow/base.h"
+
+using namespace reffine;
 
 ArrowArray* make_vector()
 {
-    return nullptr;
+    size_t len = 10000;
+
+    auto* arr = new VectorArray(len);
+    arr->add_child(new Int64Array(len));
+
+    return arr;
 }
